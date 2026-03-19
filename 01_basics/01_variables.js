62533c89b20f41c7864a60b5474bc3ddd35cf73a
sangeethaprasadg <sangeethaprasadg@gmail.com>
2026-03-19 04:34:10 +0000

    js variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "sangee@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "sangee2@outlook.com"
+accountPassword ="1111111"
+accountCity ="tvm"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// preferred to use let instead of let and const
+//  because of problem with block scope and functional scope
