423f0a9dc7c08acf2774f10796b0971dc9a45c3e
sangeethaprasadg <sangeethaprasadg@gmail.com>
2026-03-19 09:09:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict";// treat all js code as newer version
+
+
+// alert(3+3) //we are using nodejs not browser
+
+console.log(3
+    +3) ; //code readability should be high
+
+console.log("sangee");//
+
+
+let name = "sangee" //string datatype
+let age = 22 
+let isLogged = false
+
+
+//primitive datatype
+//number => 2 to power 53
+//BigInt =>stock 
+//string =>""
+//boolean => true /false
+//null=> standalone value ;it is representation of empty value
+//undefined => not given a value
+//symbol =>it is used find unquieness 
+
+//non primitive datatype
+//object
+
+
+console.log(typeof null);//object (output)
+console.log(typeof undefined);//undefined
+
+
+
+
